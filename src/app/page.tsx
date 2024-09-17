5948e0c968da88b8e5febe07918f07ca8d8dfe8a
naya-h2 <hw7666@sogang.ac.kr>
2024-09-17 22:52:20 +0900

    chore: set design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import tw from "tailwind-styled-components";
+import tw from 'tailwind-styled-components';
 
 export default function Home() {
   const MainWrapper = tw.main`
@@ -12,8 +12,9 @@ export default function Home() {
   `;
   return (
     <MainWrapper>
-      <h1 className="text-4xl font-bold">GDSC Sogang</h1>
-      <p className="text-lg text-gray-600">2024.09.02 ~</p>
+      <h1 className='text-gdsc-Blue-100 text-H1_EN'>GDSC Sogang H1_EN 영문 폰트</h1>
+      <h2 className='text-gdsc-Blue-100 text-H2_KR'>GDSC Sogang H2_KR 한글 폰트</h2>
+      <p className='text-gdsc-Red-100 text-P1_EN'>2024.09.02 ~</p>
     </MainWrapper>
   );
 }
