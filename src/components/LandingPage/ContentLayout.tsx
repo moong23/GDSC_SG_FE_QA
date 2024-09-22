5486a85efef93f28948d194d45d3eb9498b10f3b
moonki <moonki0623@naver.com>
2024-09-23 01:17:42 +0900

    fix: return 분기처리 및 내용 추가
    - 버튼이 2개인경우(메인페이지) 분기처리 추가
    - 버튼 1개인 경우 (나머지) button을 Link로 변경하여 SSR환경에서 라우팅추가
    - 자연스러운 화면 전환 위해 threshold값 0.8으로 변경

--- src/components/LandingPage/ContentLayout.tsx
+++ src/components/LandingPage/ContentLayout.tsx
@@ -1,18 +1,33 @@
 'use client';
 
+import Link from 'next/link';
 import { ReactNode, useEffect, useRef, useState } from 'react';
 
-interface Props {
-  title: ReactNode;
+interface SingleButtonProps {
+  isDoubleBtn?: false;
   btnBoldText: string;
-  children: ReactNode;
+  btnClickLink: string;
 }
+
+interface DoubleButtonProps {
+  isDoubleBtn: true;
+  btn1: ReactNode;
+  btn2: ReactNode;
+}
+
+interface CommonProps extends React.PropsWithChildren {
+  title: ReactNode;
+  subtitle?: ReactNode;
+}
+
+type Props = CommonProps & (SingleButtonProps | DoubleButtonProps);
 
 /**
  * 스크롤 애니메이션이 들어가는 content의 layout
  * @param children title과 button 제외 body 영역
  */
-const ContentLayout = ({ title, btnBoldText, children }: Props) => {
+const ContentLayout = (props: Props) => {
+  const { title, subtitle, children } = props;
   const targetRef = useRef(null);
   const sectionRef = useRef(null);
   const [isVisible, setIsVisible] = useState(true);
@@ -23,7 +38,7 @@ const ContentLayout = ({ title, btnBoldText, children }: Props) => {
     const observerOptions = {
       root: null,
       rootMargin: '0px',
-      threshold: 0,
+      threshold: 0.8,
     };
 
     const observer = new IntersectionObserver(entries => {
@@ -42,26 +57,53 @@ const ContentLayout = ({ title, btnBoldText, children }: Props) => {
       }
     };
   }, []);
+  if (props.isDoubleBtn) {
+    return (
+      <div className='w-content'>
+        <div ref={targetRef}></div>
+        <section
+          ref={sectionRef}
+          className={`flex w-full flex-col gap-16 ${!isVisible && 'animate-content-out opacity-0'}`}
+        >
+          <span className='flex flex-col gap-3'>
+            <span className='flex flex-col'>
+              <h3 className={`text-H1_EN ${isVisible && 'animate-content-in'}`}>{title}</h3>
+              <h2 className={`${isVisible && 'animate-content-in'} text-h2_EN text-gdsc-Black -mt-6`}>{subtitle}</h2>
+            </span>
+            <div className={`${isVisible && 'animate-content-in-slow'}`}>{children}</div>
+          </span>
+          <div className={`flex gap-5 ${isVisible && 'animate-content-in'}`}>
+            {props.btn1}
+            {props.btn2}
+          </div>
+        </section>
+      </div>
+    );
+  }
 
   return (
-    <div className='ml-[20%] max-w-[712px]'>
+    <div className='w-content'>
       <div ref={targetRef}></div>
       <section
         ref={sectionRef}
         className={`flex w-full flex-col gap-10 ${!isVisible && 'animate-content-out opacity-0'}`}
       >
         <h3 className={`text-H1_EN ${isVisible && 'animate-content-in'}`}>{title}</h3>
+        {subtitle && (
+          <h2 className={`${isVisible && 'animate-content-in'} text-h2_EN text-gdsc-Black -mt-16`}>{subtitle}</h2>
+        )}
         <div className={`px-11 ${isVisible && 'animate-content-in-slow'}`}>{children}</div>
-        <button
+        <Link
           className={`mx-5 w-fit rounded-[35px] bg-gdsc-Black px-[26px] py-[18px] text-gdsc-White 
             ${isVisible && 'animate-content-in'}
           `}
+          href={props.btnClickLink}
         >
-          Learn more about <span className='font-bold'>{btnBoldText}</span>
-        </button>
+          Learn more about <span className='font-bold'>{props.btnBoldText}</span>
+        </Link>
       </section>
     </div>
   );
-}
+};
 
 export default ContentLayout;
