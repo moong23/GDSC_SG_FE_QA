89ab0b133d12c1ac55ad951a347e2ba0349557ae
moonki <moonki0623@naver.com>
2024-10-02 18:11:01 +0900

    feat: 프로젝트에 GA 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body className={notoSansKr.className}>{children}</body>
+      {process.env.NEXT_PUBLIC_GA_ID && <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />}
     </html>
   );
 }
